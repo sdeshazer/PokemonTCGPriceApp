24af2b41cef7a486c0cff45b09f56b996388bb05
sdeshazer <sdeshazer92@gmail.com>
2022-04-20 22:17:17 -0700

    Indexed successful, got rid of series ids, to be done from front end

---
+++ CreateTuple.py
@@ -0,0 +1,6 @@
+# creating the tuple based on schema:
+def createPokemonTuple(data):
+    result_tuple = (data['set'],
+                    data['name'],
+                    data['rarity'])
+    return (result_tuple)
