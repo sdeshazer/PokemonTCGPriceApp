e8a364d5cfa28c2c4362ff0fa0a32420b60fcfb3
sdeshazer <sdeshazer92@gmail.com>
2022-04-23 20:12:19 -0700

    SingleCardScraper.py returns a higher resolution image

--- SingleCardScraper.py
+++ SingleCardScraper.py
@@ -20,21 +20,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 option = webdriver.ChromeOptions()
 option.add_argument('headless')
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
-url = 'https://www.tcgplayer.com/product/250334/pokemon-celebrations-classic-collection-luxray-gl-lvx'
+url = 'https://www.tcgplayer.com/product/250324/pokemon-celebrations-classic-collection-rockets-zapdos?Language=English'
 csv_path_current_card = 'currentCard.csv'
 language = '?Language=English'
 
 
 def scrape_card_details(card_details_page):
-
     with open(csv_path_current_card, 'w', newline="") as csv_file:
         browser.get(url)
         # browser.find_element(By.ID, 'img')
-        src = browser.find_elements(by=By.TAG_NAME, value='img')
-        print(len(src))
-        for img in src:
-            print(img.get_attribute('src'))
+        img_list = browser.find_elements(by=By.TAG_NAME, value='img')
+        print(len(img_list))
+        card_img_src = get_card_img(img_list)
+        print(card_img_src)
 
+
+def get_card_img(img_list):
+    for img in img_list:
+        img_src = img.get_attribute('src')
+        if img_src.__contains__("product-images"):
+            return img_src
 
 
 # test single card scraper:
