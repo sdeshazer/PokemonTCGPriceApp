1c2e5a995ebc649391f41cb9e81d42e7f89308d5
sdeshazer <sdeshazer92@gmail.com>
2022-04-24 00:36:26 -0700

    able to retrieve price history and pass it as a json object.

--- SingleCardScraper.py
+++ SingleCardScraper.py
@@ -15,12 +15,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import json
+
 
 # browser = webdriver.Chrome(executable_path='chromedriver.exe')
 
 
 option = webdriver.ChromeOptions()
-# option.add_argument('headless') # allow driver to work within the background
+option.add_argument('headless') # allow driver to work within the background
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
 url = 'https://www.tcgplayer.com/product/250324/pokemon-celebrations-classic-collection-rockets-zapdos?Language=English'
 csv_path_current_card = 'currentCard.csv'
@@ -34,8 +36,8 @@ def scrape_card_details(card_details_page):
         browser.get(url)
         # browser.find_element(By.ID, 'img')
         img_list = browser.find_elements(by=By.TAG_NAME, value='img')
-        print(len(img_list))
         card_img_src = get_card_img(img_list)
+        print("image retrieved:")
         print(card_img_src)
 
 
@@ -53,20 +55,53 @@ def get_card_img(img_list):
 
 def get_card_price_history():
     try:
+        print("please wait page is loading....")
+        webdriver.Chrome.implicitly_wait(browser, time_to_wait=5)
         element = browser.find_element(by=By.CLASS_NAME, value="price-guide__latest-sales__more")
         elements = element.find_elements(by=By.TAG_NAME, value="span")
+        for el in elements:
+            el.click()
+        print("please wait price data is loading....")
+        webdriver.Chrome.implicitly_wait(browser, time_to_wait=5)
+        # browser.implicitly_wait(browser,5)
+        #  element = browser.find_element(by=By.CLASS_NAME, value="price-guide-modal__load-more")
+        element = browser.find_element(by=By.CLASS_NAME, value="modal-slide__scale")
+        elements = element.find_elements()
+        #print("any elements : " + str(len(elements)))
+        for el in elements:
+            el.click()
+        #elements = browser.find_elements(by=By.TAG_NAME, value="li")
+        #print(len(elements))
 
-       # el.click()
+        date_elements  = browser.find_elements(by=By.CLASS_NAME, value="date")
+        price_elements = browser.find_elements(by=By.CLASS_NAME, value="price")
+        date_list = []
+        price_list = []
+        for el in  price_elements:
+            price_list.append(el.text.strip())
+        for el in date_elements:
+            date_list.append(el.text.strip())
+
+        listDP = list(map(lambda x, y: "date:" + x + ',' + "price:" + y, date_list, price_list))
+        # returns it as a json object for the front end:
+        print("returning json object of price history\n", json.dumps(listDP))
+        return json.dumps(listDP)
 
     except Exception as e:
         print("err: closing chrome driver process with exit : " + str(e))
-        browser.close()
-        browser.quit()
+        quit_browsing()
 
+# python does not handle exiting the processes in the background
+def quit_browsing():
+    browser.close()
+    browser.quit()
 
 # test single card scraper:
 if __name__ == '__main__':
-    scrape_card_details(url)
-    get_card_price_history()
-    browser.close()
-    browser.quit()  # python does not handle exiting the processes in the background.
+    try:
+        scrape_card_details(url)
+        get_card_price_history()
+    except Exception as e:
+        print("err: closing chrome driver process with exit : " + str(e))
+        quit_browsing()
+    quit_browsing()
